5ceb6a8a21fee167e9dc57c1830d9bb5782f1a68
Chirag Aggarwal <aggarwalc@mx.lakeforest.edu>
2022-05-25 11:27:48 -0500

    add Date column

--- frameworks/frameworks.sql
+++ frameworks/frameworks.sql
@@ -1,19 +1,19 @@
 WITH main AS (SELECT post_year,
-       post_month,
-       tag,
-       month_count -- optional, not needed in main table
+                     post_month,
+                     tag,
+                     month_count -- optional, not needed in main table
 
 FROM (
 
-  SELECT datepart(year,Posts.CreationDate) as post_year,
-         datepart(month,Posts.CreationDate) as post_month,
-         tags.TagName as tag,
-         COUNT(*) as month_count
+  SELECT datepart(year,Posts.CreationDate) AS post_year,
+         datepart(month,Posts.CreationDate) AS post_month,
+         tags.TagName AS tag,
+         COUNT(*) AS month_count
 
-  FROM PostTags as pt
+  FROM PostTags AS pt
     INNER JOIN Posts
     ON pt.PostId = Posts.Id
-    INNER JOIN Tags as tags
+    INNER JOIN Tags AS tags
     ON pt.TagId = tags.Id
 
   WHERE tags.TagName in ('express', 'django', 'ruby-on-rails',
@@ -32,24 +32,25 @@ GROUP BY post_year,
          tag,
          month_count),
 
-sub AS (SELECT datepart(year,Posts.CreationDate) as post_year,
-               datepart(month,Posts.CreationDate) as post_month,
-               count(*) as total
+sub AS (SELECT datepart(year,Posts.CreationDate) AS post_year,
+               datepart(month,Posts.CreationDate) AS post_month,
+               count(*) AS total
         FROM Posts WHERE PostTypeId = 1
         GROUP BY datepart(year,Posts.CreationDate),
                  datepart(month,Posts.CreationDate))
 
 
-SELECT main.post_year as 'year',
-       main.post_month as 'month',
-       tag as 'tag',
-       month_count as 'month count per tag',
-       total as 'overall count',
-       month_count * 100.0 / total as 'percent',
+SELECT main.post_year AS 'year',
+       main.post_month AS 'month',
+       DATEFROMPARTS(main.post_year, main.post_month, 1) AS 'date',
+       tag AS 'tag',
+       month_count AS 'month_count_per_tag',
+       total AS 'overall_count',
+       month_count * 100.0 / total AS 'percent',
        SUM(month_count) OVER(PARTITION BY tag
                              ORDER BY main.post_year,
                                       main.post_month
-                             ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) as 'running count'
+                             ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS 'running_count'
 FROM main INNER JOIN sub ON main.post_year = sub.post_year
                          WHERE main.post_month = sub.post_month
 ORDER BY main.post_year,
--- machine-learning-libraries/machine_learning_libraries.sql
+++ machine-learning-libraries/machine_learning_libraries.sql
@@ -16,8 +16,63 @@ FROM (
     INNER JOIN Tags as tags
     ON pt.TagId = tags.Id
 
+  WHERE tags.TagName in WITH main AS (SELECT post_year,
+                     post_month,
+                     tag,
+                     month_count -- optional, not needed in main table
+
+FROM (
+
+  SELECT datepart(year,Posts.CreationDate) AS post_year,
+         datepart(month,Posts.CreationDate) AS post_month,
+         tags.TagName AS tag,
+         COUNT(*) AS month_count
+
+  FROM PostTags AS pt
+    INNER JOIN Posts
+    ON pt.PostId = Posts.Id
+    INNER JOIN Tags AS tags
+    ON pt.TagId = tags.Id
+
   WHERE tags.TagName in ('tensorflow', 'pytorch', 'scipy',
                          'keras', 'scikit-learn', 'numpy', 'pandas')
+  AND Posts.PostTypeId = 1
+  GROUP BY datepart(year,Posts.CreationDate),
+           datepart(month,Posts.CreationDate),
+           tags.TagName
+
+     ) AS subquery
+
+
+GROUP BY post_year,
+         post_month,
+         tag,
+         month_count),
+
+sub AS (SELECT datepart(year,Posts.CreationDate) AS post_year,
+               datepart(month,Posts.CreationDate) AS post_month,
+               count(*) AS total
+        FROM Posts WHERE PostTypeId = 1
+        GROUP BY datepart(year,Posts.CreationDate),
+                 datepart(month,Posts.CreationDate))
+
+
+SELECT main.post_year AS 'year',
+       main.post_month AS 'month',
+       DATEFROMPARTS(main.post_year, main.post_month, 1) AS 'date',
+       tag AS 'tag',
+       month_count AS 'month_count_per_tag',
+       total AS 'overall_count',
+       month_count * 100.0 / total AS 'percent',
+       SUM(month_count) OVER(PARTITION BY tag
+                             ORDER BY main.post_year,
+                                      main.post_month
+                             ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS 'running_count'
+FROM main INNER JOIN sub ON main.post_year = sub.post_year
+                         WHERE main.post_month = sub.post_month
+ORDER BY main.post_year,
+         main.post_month,
+         tag;
   AND Posts.PostTypeId = 1
   GROUP BY datepart(year,Posts.CreationDate),
            datepart(month,Posts.CreationDate),
