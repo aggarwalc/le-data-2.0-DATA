9e82403c1432a8ee8e2d3754bf9c43d6e1ccaaa5
Chirag Aggarwal <aggarwalc@mx.lakeforest.edu>
2022-07-04 14:49:04 -0500

    job listings data

--- Indeed-LinkedIn/job_listings.py
+++ Indeed-LinkedIn/job_listings.py
@@ -10,7 +10,8 @@ languages = {'C#':'C%23', 'C++':'C%2B%2B', 'Java':'Java',
              'JavaScript':'JavaScript', 'PHP':'PHP', 'Python':'Python',
              'Ruby':'Ruby', 'Swift':'Swift', 'Kotlin':'Kotlin',
              'Dart':'Dart', 'Objective-C':'Objective-C', 'R':'R',
-             'MATLAB':'MATLAB', 'Julia':'Julia', 'Fortran':'Fortran'}
+             'MATLAB':'MATLAB', 'Julia':'Julia', 'Fortran':'Fortran',
+             'SPSS':'SPSS%20statistical', 'SAS':'SAS%20statistical'}
 
 for lang in languages:
     step += 1
@@ -24,6 +25,6 @@ for lang in languages:
     df = df.append({'value':LinkedIn_count, 'language':lang}, ignore_index=True)
 
 df = df.sort_values(by='value', ascending=False, ignore_index=True)
-df['step'] = list(range(1, 16))
+df['step'] = list(range(1, 18))
 
-df.to_csv('job_listings_results.csv', index=False)
+df.to_csv('job_listings_results_7_02.csv', index=False)
